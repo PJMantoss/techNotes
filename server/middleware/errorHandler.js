5949ce84ef47a56248861a1e152585d1afe8b5e7
PJMantoss <joelptoss@gmail.com>
2022-11-01 17:58:24 +0100

    update errorHandler

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -3,4 +3,6 @@ const { logEvents } = require("./logger");
 const errorHandler = (err, req, res, next) => {
     logEvents(`${err.name}\t${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, "errLog.log");
     console.log(err.Stack);
+
+    const status = res.statusCode ? res.statusCode : 500 // server error
 };

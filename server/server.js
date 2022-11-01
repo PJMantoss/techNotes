cf4ad42a5b696787ebce043b094654022739ebd9
PJMantoss <joelptoss@gmail.com>
2022-11-01 16:26:41 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
-app.use('/', require('./routes/root'));
+app.use("/", require("./routes/root"));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

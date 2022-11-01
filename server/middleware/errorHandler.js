487c462fab4b1d509c2135f181bfdfbed489e639
PJMantoss <joelptoss@gmail.com>
2022-11-01 17:55:31 +0100

    update errorHandler

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,1 +1,6 @@
 const { logEvents } = require("./logger");
+
+const errorHandler = (err, req, res, next) => {
+    logEvents(`${err.name}\t${}\t${}\t${}\t${}`, "errLog.log");
+    console.log(err.Stack);
+};

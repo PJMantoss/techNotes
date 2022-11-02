3d4c1325c276dec1ea4091c1888f199f86794395
PJMantoss <joelptoss@gmail.com>
2022-11-02 12:40:31 +0100

    update config

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     "http://localhost:3500/",
     "http://www.kvrepairshop.com/",
-    "http://kvrepairshop.com/"
+    "http://kvrepairshop.com/",
+    "https://www.google.com"
 ];
 
 module.exports = allowedOrigins;

ec754c705cd2889e9f2f0c5c42882995a3b39305
PJMantoss <joelptoss@gmail.com>
2022-11-02 14:52:55 +0100

    update User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,20 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    active: {
+        type: Boolean,
+        default: true
+    },
+});
+
+module.exports = userSchema;

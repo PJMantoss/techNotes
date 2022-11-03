308c8b47cf5a0d3ec62a16f760f01774e5ebd8e4
PJMantoss <joelptoss@gmail.com>
2022-11-03 14:25:38 +0100

    update users controller

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -9,14 +9,41 @@ const bcrypt = require("bcrypt"); //For hashing passwords in the database
 const getAllUsers = asyncHandler(async = (req, res) => {
     const users = await User.find().select("-password").lean();
     if(!users){
-        return res.status(400).json({ message: "User Not Found!" })
+        return res.status(400).json({ message: "Users Not Found!" })
     }
+    res.json(users);
 });
 
 // @desc - Create new users
 // @route POST /users 
 // @access Private
-const createNewUser = asyncHandler(async = (req, res) => {});
+const createNewUser = asyncHandler(async = (req, res) => {
+    const { username, password, roles } = req.body;
+
+    // Confirm Data
+    if(!username || !password || !Array.isArray(roles) || !roles.length){
+        return res.status(400).json({ message: "All fields are required" });
+    };
+
+    // Check for Duplicates
+    const duplicate = await User.findOne({ username }).lean().exec();
+
+    if(duplicate){
+        return res.status(409).json({ message: "Duplicate username" });
+    }
+
+    // Hash Password
+    const hashedPassword = await bcrypt.hash(password, 10); // salt rounds
+    
+    const userObj = {
+        username,
+        "password": hashedPassword,
+        roles
+    };
+
+    // Create and store new user
+    const user = await User.create(userObj);
+ });
 
 // @desc - Update user
 // @route PATCH /users 

c3d7205206cf11add2c1cb1b910994ff5dc2bd5c
PJMantoss <joelptoss@gmail.com>
2022-11-03 14:09:58 +0100

    update users controller

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -6,7 +6,12 @@ const bcrypt = require("bcrypt"); //For hashing passwords in the database
 // @desc - GET All users
 // @route GET /users 
 // @access Private
-const getAllUsers = asyncHandler(async = (req, res) => {});
+const getAllUsers = asyncHandler(async = (req, res) => {
+    const users = await User.find().select("-password").lean();
+    if(!users){
+        return res.status(400).json({ message: "User Not Found!" })
+    }
+});
 
 // @desc - Create new users
 // @route POST /users 

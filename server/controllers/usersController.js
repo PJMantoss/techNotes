5ce831da00dfa5f0b40d6bb4f8439b2973d12d0c
PJMantoss <joelptoss@gmail.com>
2022-11-03 13:59:29 +0100

    update user Routes & controller

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,2 +1,24 @@
 const User = require("../models/User");
 const Note = require("../models/Note");
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt"); //For hashing passwords in the database
+
+// @desc - GET All users
+// @route GET /users 
+// @access Private
+const getAllUsers = asyncHandler(async = (req, res) => {});
+
+// @desc - Create new users
+// @route POST /users 
+// @access Private
+const createNewUsers = asyncHandler(async = (req, res) => {});
+
+// @desc - Update user
+// @route PATCH /users 
+// @access Private
+const updateUser = asyncHandler(async = (req, res) => {});
+
+// @desc - Delete user
+// @route DELETE /users 
+// @access Private
+const deleteUser = asyncHandler(async = (req, res) => {});
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,22 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcrypt");
 
 router.route("/")
     .get()
     .post()
     .patch()
     .delete();
-
-// @desc - GET All users
-// @route GET /users 
-// @access Private
-const getAllUsers = asyncHandler(async = (req, res) => {});
-
-// @desc - Create new users
-// @route POST /users 
-// @access Private
-const createNewUsers = asyncHandler(async = (req, res) => {});
 
 module.exports = router;

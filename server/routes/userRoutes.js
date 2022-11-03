6fda4c0833719c903d1036ba95262e1ab9c8e0f9
PJMantoss <joelptoss@gmail.com>
2022-11-03 13:47:34 +0100

    update userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
 
 router.route("/")
     .get()
     .post()
     .patch()
     .delete();
+
+// @desc - GET All users
+// @route GET /users 
+// @access private
+const getAllUsers = asyncHandler(async = () => {})
 
 module.exports = router;

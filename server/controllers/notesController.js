22f3522bae1cfaf4f4c66b239c1fc659a688b61c
PJMantoss <joelptoss@gmail.com>
2022-11-04 18:32:17 +0100

    update notes controller

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -1,3 +1,15 @@
 const User = require("../models/User");
 const Note = require("../models/Note");
 const asyncHandler = require("express-async-handler");
+
+// @desc - GET All notes
+// @route GET /notes 
+// @access Private
+const getAllNotes = asyncHandler(async (req, res) => {});
+
+module.exports = {
+    getAllNotes,
+    createNewNote,
+    updateNote,
+    deleteNote
+};

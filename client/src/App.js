6ba50f1521fc190dc661bd011dd11382813e2316
PJMantoss <joelptoss@gmail.com>
2022-11-06 19:25:40 +0100

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div></div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        
+      </Route>
+    </Routes>
   );
 }
 

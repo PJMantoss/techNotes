79700cd605cddc7a20c1d0f20201ac48be926c51
PJMantoss <joelptoss@gmail.com>
2022-11-06 19:37:55 +0100

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
-        <Route path="/" element={<Login />} />
+        <Route path="login" element={<Login />} />
       </Route>
     </Routes>
   );

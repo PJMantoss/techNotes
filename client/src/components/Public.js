5133cca55d4b2ffd6605356db385fafd483beb5c
PJMantoss <joelptoss@gmail.com>
2022-11-06 19:31:56 +0100

    update Public component

--- client/src/components/Public.js
+++ client/src/components/Public.js
@@ -2,9 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Public = () => {
-  return (
-    <div>Public</div>
-  )
+    const content = (
+        <section className="public">
+            <header>
+                <h1>Welcome to <span className="nowrap">K V. Repairs!</span></h1>
+            </header>
+            <main className="public__main">
+                <p>Located in Jos City, K V. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
+                <address className="public__addr">
+                    K V. Repairs<br />
+                    555 Foo Drive<br />
+                    Jos City, PL 12345<br />
+                    <a href="tel:+15555555555">(555) 555-5555</a>
+                </address>
+                <br />
+                <p>Owner: Dan Davidson</p>
+            </main>
+            <footer>
+                <Link to="/login">Employee Login</Link>
+            </footer>
+        </section>
+
+    )
+    return content
 }
 
 export default Public

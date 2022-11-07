77169e0e304ca2f886b5318ec0f40386d97353dc
PJMantoss <joelptoss@gmail.com>
2022-11-07 20:03:27 +0100

    add DashHeader

--- client/src/components/DashHeader.js
+++ client/src/components/DashHeader.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const DashHeader = () => {
-  return (
-    <div>DashHeader</div>
-  )
+
+    const content = (
+        <header>
+            <div>
+                <Link></Link>
+                <nav></nav>
+            </div>
+        </header>
+    );
+
+  return content;
 }
 
 export default DashHeader;

0f9c582615af42ca0720545967a77a0ecf0afeae
PJMantoss <joelptoss@gmail.com>
2022-11-09 17:30:59 +0100

    update apiSLice

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -1,4 +1,4 @@
-import { cretateApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),

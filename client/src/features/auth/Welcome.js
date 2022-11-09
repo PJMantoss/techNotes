dfb9e17429197239174e1b2591e3977e596668b7
PJMantoss <joelptoss@gmail.com>
2022-11-09 15:05:56 +0100

    add Welcome component in auth dir

--- client/src/features/auth/Welcome.js
+++ client/src/features/auth/Welcome.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Welcome = () => {
+    const date = new Date();
+    const today = new Intl.DateTimeFormat().format(date);
   return (
     <div>Welcome</div>
   )

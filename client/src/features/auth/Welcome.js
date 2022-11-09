672a074a5d0b31803cb975b416deb282a22bf004
PJMantoss <joelptoss@gmail.com>
2022-11-09 15:11:30 +0100

    update Welcome

--- client/src/features/auth/Welcome.js
+++ client/src/features/auth/Welcome.js
@@ -4,7 +4,23 @@ import { Link } from 'react-router-dom';
 const Welcome = () => {
     const date = new Date();
     const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date);
-  return (
+    const content = (
+        <section>
+            <p>{today}</p>
+            <h1>Welcome!</h1>
+            <p>
+                <Link to="/dash/notes">
+                    View TechNotes
+                </Link>
+            </p>
+            <p>
+                <Link to="/dash/users">
+                    View User Settings
+                </Link>
+            </p>
+        </section>
+    );
+    return (
     <div>Welcome</div>
   )
 }

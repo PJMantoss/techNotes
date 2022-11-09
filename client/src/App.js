bce0c6ecff603cad100e4d55a848c41dece2ba2c
PJMantoss <joelptoss@gmail.com>
2022-11-09 15:59:39 +0100

    update App

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,23 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
+
+        {/* Protected Route - Dashboard Layout */}
         <Route path="dash" element={<DashLayout />}>
+          
           <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+
         </Route>
+        {/* END - Protected Route */}
+
       </Route>
     </Routes>
   );

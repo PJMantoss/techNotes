7310166af6f7619a91a389e98852330794f5a94b
PJMantoss <joelptoss@gmail.com>
2022-11-11 17:57:25 +0100

    update NoteList

--- client/src/features/notes/NotesList.js
+++ client/src/features/notes/NotesList.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { useGetUSersQuery } from '../users/usersApiSlice';
 
 const NotesList = () => {
+  const {
+    data: notes,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetUSersQuery();
+
+  let content
+
+  if(isLoading) content = <p>Loading...</p>
+
+  if(isError){
+    content = <p className="errmsg"> {error?.data?.message} </p>
+  }
+
   return (
     <h1>NotesList</h1>
   )

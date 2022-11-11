af0fb70d6f161d40f8afbd31aaf1a6ad33f6ad28
PJMantoss <joelptoss@gmail.com>
2022-11-11 17:42:06 +0100

    update User

--- client/src/features/users/User.js
+++ client/src/features/users/User.js
@@ -8,6 +8,8 @@ import { selectUserById } from './usersApiSlice';
 import React from 'react'
 
 const User = ({ userId }) => {
+    const user = useSelector();
+    
   return (
     <div>User</div>
   )

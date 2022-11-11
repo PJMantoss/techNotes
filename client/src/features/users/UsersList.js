929b8adab726e12c443d8254e5147f48a317f63e
PJMantoss <joelptoss@gmail.com>
2022-11-11 17:27:38 +0100

    update users list

--- client/src/features/users/UsersList.js
+++ client/src/features/users/UsersList.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useGetUSersQuery } from './usersApiSlice';
 
 const UsersList = () => {
+
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetUSersQuery;
+
   return (
     <h1>UsersList</h1>
   )

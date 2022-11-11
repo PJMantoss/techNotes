62144fac7f40fa8966061fbe79283fd79ea93b69
PJMantoss <joelptoss@gmail.com>
2022-11-11 18:00:11 +0100

    update NoteList

--- client/src/features/notes/NotesList.js
+++ client/src/features/notes/NotesList.js
@@ -18,9 +18,33 @@ const NotesList = () => {
     content = <p className="errmsg"> {error?.data?.message} </p>
   }
 
-  return (
-    <h1>NotesList</h1>
-  )
+  if (isSuccess) {
+    const { ids } = notes
+
+    const tableContent = ids?.length
+        ? ids.map(noteId => <Note key={noteId} noteId={noteId} />)
+        : null
+
+    content = (
+        <table className="table table--notes">
+            <thead className="table__thead">
+                <tr>
+                    <th scope="col" className="table__th note__status">Username</th>
+                    <th scope="col" className="table__th note__created">Created</th>
+                    <th scope="col" className="table__th note__updated">Updated</th>
+                    <th scope="col" className="table__th note__title">Title</th>
+                    <th scope="col" className="table__th note__username">Owner</th>
+                    <th scope="col" className="table__th note__edit">Edit</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tableContent}
+            </tbody>
+        </table>
+    )
+}
+
+  return content;
 }
 
 export default NotesList;

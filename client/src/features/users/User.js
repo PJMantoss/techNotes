b838456bbd8a7ad2a0d3e1128f71802d9ba82830
PJMantoss <joelptoss@gmail.com>
2022-11-11 17:48:20 +0100

    update User

--- client/src/features/users/User.js
+++ client/src/features/users/User.js
@@ -13,11 +13,27 @@ const User = ({ userId }) => {
     const navigate = useNavigate();
 
     if(user){
-        const handleEdit = () => navigate(`/dash/users/${userId}`)
+        const handleEdit = () => navigate(`/dash/users/${userId}`);
 
-        const userRolesString = user.roles.toString().replaceAll(',', ', ')
+        const userRolesString = user.roles.toString().replaceAll(',', ', ');
 
-        const cellStatus = user.active ? '' : 'table__cell--inactive'
+        const cellStatus = user.active ? '' : 'table__cell--inactive';
+
+        return (
+            <tr className="table__row user">
+                <td className={`table__cell ${cellStatus}`}>{user.username}</td>
+                <td className={`table__cell ${cellStatus}`}>{userRolesString}</td>
+                <td className={`table__cell ${cellStatus}`}>
+                    <button
+                        className="icon-button table__button"
+                        onClick={handleEdit}
+                    >
+                        <FontAwesomeIcon icon={faPenToSquare} />
+                    </button>
+                </td>
+            </tr>
+        )
+
 
     } else return null
 

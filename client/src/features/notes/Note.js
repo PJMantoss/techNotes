7c709f8a660be814f797c2b405c4dc8e2c367b3f
PJMantoss <joelptoss@gmail.com>
2022-11-11 18:02:46 +0100

    add Note

---
+++ client/src/features/notes/Note.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+
+const Note = () => {
+  return (
+    <div>Note</div>
+  )
+}
+
+export default Note

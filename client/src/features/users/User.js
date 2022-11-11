b1c0aab0b9e561e73211d14599755e7e1f92f9a2
PJMantoss <joelptoss@gmail.com>
2022-11-11 17:54:58 +0100

    update User

--- client/src/features/users/User.js
+++ client/src/features/users/User.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
 import { selectUserById } from './usersApiSlice';
